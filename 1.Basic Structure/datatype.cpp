60a92fbf4bc01297ad296215d74969d3b2198ef4
zafar-03 <zfnanshabhai@gmail.com>
2026-07-29 15:53:47 +0530

    Introduction of CPP

---
+++ 1.Basic Structure/datatype.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int num1 = 100;
+    char ch = 'A';
+    float pi = 3.14;
+    string fname = "Raj";
+    // numeric all number without points
+
+    cout << "Number is : " << num1 << endl;
+    cout << "Char is : " << ch << endl;
+    cout << "Value of PI : " << pi << endl;
+    cout << "Your name is :" << fname << endl;
+
+    return 0;
+}
+
+// printf("Number is  : %d",num1)
---
+++ 1.Basic Structure/userinput.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int age;
+    string fname;
+
+    cout << "Enter your Age : ";
+    cin >> age;
+    cout << "Enter Your name : ";
+    cin >> fname;
+
+
+    cout << "Your name is " << fname << ".and YOur age is " << age << "." ;
+
+    return 0;
+}
+
+/*
+user Input : 
+    firsrname
+    lastname
+    age
+    english marks
+    maths marks
+    science marks
+    
+    print : 
+    in a Single Statement : firsrname lastname age
+
+    % : 23+45+56/3
+
+
+*/
